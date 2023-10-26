b8f51ff6a62fcd83d5c3947c18f413fdd3e52ab4
julian <juliangrisales5@gmail.com>
2023-10-26 13:54:55 -0500

    ejercicio3.1

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 /*
 const express = require('express');//importa le modulo express
-const app = express();//crea la instancia de express
-const port = 3000; //se define el puerto
-
+const app == 3000; //se define el puerto
+ express();//crea la instancia de express
+const port 
 //definir una ruta usando app.get()
 app.get('/', (req, res) =>{//define la ruta / (raiz) para las peticiones get
     res.send('Hello Express');//se ennvia la respuesta
