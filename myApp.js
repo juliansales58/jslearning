2780ba8eb6d1241730c6a4fcf6785d9121c735c4
julian <juliangrisales5@gmail.com>
2023-10-26 08:36:18 -0500

    ejercicio3

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');//importa le modulo express
 const app = express();//crea la instancia de express
 const port = 3000; //se define el puerto
@@ -11,6 +12,11 @@ app.get('/', (req, res) =>{//define la ruta / (raiz) para las peticiones get
 app.listen(port, ()=> {//pone a escuchar el puerto
     console.log('Servidor Express escuchando el puerto ${port}');
 }); 
+*/
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+  });
 
 
 
